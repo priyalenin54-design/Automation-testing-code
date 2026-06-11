50adb4858e07babde309a6c09713a32b33235f33
priyalenin54-design <priya.lenin54@gmail.com>
2026-06-11 12:06:43 +0530

    initial project commit

---
+++ src/test/java/seleniumPractice/BasicsMethods.java
@@ -0,0 +1,25 @@
+package seleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BasicsMethods {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver= new EdgeDriver();  //upcasting
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("name")).sendKeys("Priya");
+		driver.findElement(By.id("email")).sendKeys("dharsinipriya1991@gmail.com");
+		driver.findElement(By.id("phone")).sendKeys("7401410546");
+		driver.findElement(By.id("textarea")).sendKeys("NO-205,Perungalathur,chennai-600063");
+		
+		
+	}
+	
+	
+	
+	
+}
